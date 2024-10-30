dc354c410097f66330178fb6aaed8e0dd9200136
maximmc <maximmc@list.ru>
2024-10-30 20:59:13 +0300

    Initial commit

---
+++ src/main/java/ru/cherkasov/automatic_lab/AutomaticLabApplication.java
@@ -0,0 +1,13 @@
+package ru.cherkasov.automatic_lab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutomaticLabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutomaticLabApplication.class, args);
+	}
+
+}
